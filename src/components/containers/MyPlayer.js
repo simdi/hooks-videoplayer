c015f756400cc69967e548fa97bd339d80fd4672
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-02-20 06:40:33 +0100

    Boostraped Myplayer component.

---
+++ src/components/containers/MyPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Video from './Video';
+import Playlist from './Playlist';
+
+const MyPlayer = props => {
+  return (
+    <>
+      <Video />
+      <Playlist />
+    </>
+  )
+}
+
+export default MyPlayer;
